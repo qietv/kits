45a3c15565bfb5bda15c952bd206e2310c9cbdf1
guohao <i@icool.io>
2021-07-23 19:36:05 +0800

    yahaha

---
+++ utils/logger.go
@@ -0,0 +1,42 @@
+package utils
+
+import "git.qietv.work/go-public/logkit"
+var (
+    logger Logger
+)
+type Logger interface {
+    Debug(format string, args ...interface{})
+    Info(format string, args ...interface{})
+    Warn(format string, args ...interface{})
+    Error(format string, args ...interface{})
+}
+
+type SimpleLogger struct {}
+
+func NewSimpleLogger() Logger {
+    logger = &SimpleLogger{}
+    return logger
+}
+func SetLogger(l Logger)  {
+    logger = l
+}
+
+func GetLogger() Logger  {
+    return logger
+}
+
+func (s *SimpleLogger) Debug(format string, args ...interface{}) {
+    logkit.Debugf(format, args ...)
+}
+
+func (s *SimpleLogger) Info(format string, args ...interface{}) {
+    logkit.Infof(format, args ...)
+}
+
+func (s *SimpleLogger) Warn(format string, args ...interface{}) {
+    logkit.Warnf(format, args ...)
+}
+
+func (s *SimpleLogger) Error(format string, args ...interface{}) {
+    logkit.Errorf(format, args ...)
+}
